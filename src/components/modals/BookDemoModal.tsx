09147a1e42d12186731720ea70ac9fbcac29077d
veerpavan23 <veerpavan23@gmail.com>
2026-08-06 14:22:07 +0530

    fix: pass alternative RecordType parameters to fix Salesforce routing

--- src/components/modals/BookDemoModal.tsx
+++ src/components/modals/BookDemoModal.tsx
@@ -70,6 +70,8 @@ export const BookDemoModal: React.FC = () => {
       payload.append('00Nfw00000Gnb3d', formData.preferredTime);
       payload.append('lead_source', 'Web');
       payload.append('recordType', '0125g000002LdEx');
+      payload.append('RecordType', '0125g000002LdEx');
+      payload.append('RecordTypeId', '0125g000002LdEx');
 
       await fetch('https://webto.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8', {
         method: 'POST',
